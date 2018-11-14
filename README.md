ef9ee43ced57e6ba643a3d7ffbf23125fd762ce2
KnowledgeableKangaroo <31084854+KnowledgeableKangaroo@users.noreply.github.com>
2018-11-13 20:07:42 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Preventing-SQL-Injection-to-Secure-Databases
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset = "utf-8">
+	<title>Preventing SQL Injection to Secure Databases</title>
+	<link href = "css/style.css" type = "text/css" rel = "stylesheet">
+</head>
+<body>
+	<form>
+		
+	</form>
+
+	<script src = "scripts/script.js"></script>
+</body>
+</html>
