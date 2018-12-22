0992d64f7f31773e0bf01026a7ca2e6535cfb6af
KnowledgeableKangaroo <31084854+KnowledgeableKangaroo@users.noreply.github.com>
2018-12-21 21:05:30 -0500

    Created login, register
    Adapted from Larry Ullman's
    PHP and MySQL for Dynamic Web Sites
    Visual QuickPro Guide
    Fifth Edition
    
    Co-Authored-By: Larry Ullman <larryullman@users.noreply.github.com>

---
+++ pages/login.php
@@ -0,0 +1,18 @@
+<h1>Login</h1>
+<form action = "" method = "post">
+	<fieldset>
+		<legend>Login Details</legend>
+		<p>
+			<label>Email Address: </label>
+			<input type = "email" name = "email" size = "20" maxlength = "60" value = "<?php if (isset($_POST['email'])) echo $_POST['email']; ?>">
+		</p>
+		<p>
+			<label>Password: </label>
+			<input type = "password" name = "pass1" size = "10" maxlength = "20" value = "<?php if (isset($_POST['pass'])) echo $_POST['pass']; ?>">
+		</p>
+	</fieldset>
+	<p>
+		<input type = "submit" name = "submit" value = "Login">
+	</p>
+</form>
+
