2957b0532d8e495d564e0d2b0e6bed0035d7475f
Varun <31084854+KnowledgeableKangaroo@users.noreply.github.com>
2019-04-24 21:44:09 -0400

    Complete vote functionality (including answers) with AJAX; complete answer functionality (without AJAX) (#9)
    * Update question.php
    
    * Completed answer functionality, voting functionality
    
    There was a bug in the answer functionality
    Voting functionality complete - performs query
    Soon to use ajax for answering

--- pages/question.php
+++ pages/question.php
@@ -1,10 +1,13 @@
 <?php
 	file_exists('../../mysqli_connect.inc.php') ? require_once('../../mysqli_connect.inc.php') : require_once('../../Users/VSpoe/mysqli_connect.inc.php');
+	include('includes/login_functions.inc.php');
+
 	// Generate query for question's information
-	$query = 'SELECT messages.votes AS votes, messages.id AS msg_id, messages.subject AS subject, messages.body AS ques_body, users.profile_picture AS ques_profile_pic, users.first_name AS ques_asker FROM messages JOIN users ON messages.user_id = users.id WHERE messages.id = ' . $_GET['id'];
+	$query = 'SELECT messages.id AS msg_id, messages.votes AS votes, messages.subject AS subject, messages.body AS ques_body, users.profile_picture AS ques_profile_pic, users.first_name AS ques_asker FROM messages JOIN users ON messages.user_id = users.id WHERE messages.id = ' . $_GET['id'];
 	$result = mysqli_query($dbc, $query);
 	$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-
+	$query2 = 'SELECT messages.id AS msg_id, messages.votes AS votes, messages.body AS msg_body, users.profile_picture AS profile, users.first_name AS fn FROM messages JOIN users ON users.id = messages.user_id WHERE parent_id = ' . $_GET['id'];
+	$result2 = mysqli_query($dbc, $query2);
 
 	$page_title = $row['subject'];
 	include('includes/header.html');
@@ -15,11 +18,22 @@
 
 	<div class = "vote col-sm-2">
 		<?php 
-			$cur_votes = $row['votes'];
 			$ques_id = $row['msg_id'];			
-			echo "<button type = 'button' onclick = 'loadXMLDoc(\"up\")'>Vote Up</button>";
-			echo "<br><span id = 'ques-vote-count'>$cur_votes</span>";
-			echo "<br><button type = 'button' onclick = 'loadXMLDoc(\"down\")'>Vote Down</button>";
+			echo "<button id = 'vote-up-btn' type = 'button' onclick = \"loadXMLDoc('up', $ques_id, 'ques-vote-count')\">Vote Up</button>\n";
+			echo "\t\t<br><span id = 'ques-vote-count'>{$row['votes']}</span>\n";
+			echo "\t\t<br><button id = 'vote-down-btn' type = 'button' onclick = \"loadXMLDoc('down', $ques_id, 'ques-vote-count')\">Vote Down</button>\n";
+
+			$counter = 1;
+			while ($row2 = mysqli_fetch_array($result2, MYSQLI_ASSOC)) {
+				echo "\t\t<div class = 'answer'>\n";
+				echo "\t<button type = 'button' onclick = \"loadXMLDoc('up', {$row2['msg_id']}, 'ans-$counter-vote-count')\">Vote Up</button>\n";
+				echo "<br><span id = 'ans-$counter-vote-count'>{$row2['votes']}</span>";
+				echo "<br><button type = 'button' onclick = \"loadXMLDoc('down', {$row2['msg_id']}, 'ans-$counter-vote-count')\">Vote Down</button>";
+				echo "</div>";
+				$counter++;
+			}
+
+			mysqli_data_seek($result2, 0);
 		?>
 	</div>
 	<div class = "posts col-sm-8">
@@ -28,41 +42,19 @@
 			<span><?php echo $row['ques_asker'] ?></span>
 			<img height = '30' src = '../pages/show_image.php?image=<?php echo $row['ques_profile_pic']?>'>
 		</div>
-		<div class = "answers">
+		<div class = "answer-poster">
 			<?php 
 				// Generate query for answers' information
-				$query2 = 'SELECT body FROM messages WHERE parent_id = ' . $_GET['id'];
-				$result2 = mysqli_query($dbc, $query2);
 				while ($row2 = mysqli_fetch_array($result2, MYSQLI_ASSOC)) {
-					echo "<p>{$row2['body']}</p>";
+					echo "<div class = 'answer'>";
+					echo "<p>{$row2['msg_body']}</p>";
+					echo "<span>{$row2['fn']}</span>";
+					echo "<img height = '30' src = '../pages/show_image.php?image={$row2['profile']}'>";
+					echo "</div>";
 				}
 			?>
 		</div>
 	</div>
-	<?php
-
-		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-			// Validate the form elements
-			$ans = mysqli_real_escape_string($dbc, trim($_POST['answer']));
-			// Check if its okay for the user to answer the question
-			if (isset($_COOKIE['id'])) {
-				$user_id = $_COOKIE['id'];
-				$q = "SELECT id FROM messages WHERE parent_id = {$row['msg_id']} AND body = '$ans'";
-				$r = mysqli_query($dbc, $q);
-				$num = mysqli_num_rows($result);
-
-				if ($num == 0) { // No answers that match this one (no duplicates) on the current question
-					$q = "INSERT INTO messages (parent_id, user_id, body, date_entered) VALUES ({$row['id']}, $user_id, '$ans', NOW())";
-					$r = @mysqli_query($dbc, $q);
-				}
-
-
-			} else {
-				redirect_user('../Login');
-			}
-		}
-
-	?>
 </div>
 <div class = "col-sm-10">
 	<h2>Your Answer</h2>
@@ -73,8 +65,28 @@
 	</p>
 	<input type = 'submit' value = 'Post your Answer'>
 	</form>
-
 	<?php
+		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+			// Validate the form elements
+			$ans = mysqli_real_escape_string($dbc, trim($_POST['answer']));
+			// Check if its okay for the user to answer the question
+			if (isset($_COOKIE['id'])) {
+				$user_id = $_COOKIE['id'];
+				$q = "SELECT id FROM messages WHERE parent_id = $ques_id AND body = '$ans'";
+				$r = mysqli_query($dbc, $q);
+				$num = mysqli_num_rows($r);
+
+				if ($num == 0) { // No answers that match this one (no duplicates) on the current question
+					$q = "INSERT INTO messages (parent_id, user_id, body, date_entered) VALUES ({$row['msg_id']}, $user_id, '$ans', NOW())";
+					mysqli_query($dbc, $q);
+				}
+
+
+			} else {
+				redirect_user('../Login');
+			}
+		}
+
 		mysqli_close($dbc);	
 		include('includes/footer.html');
 	?>
--- pages/voteup.php
+++ pages/voteup.php
@@ -1,7 +1,7 @@
 <?php 
 	file_exists('../../mysqli_connect.inc.php') ? require_once('../../mysqli_connect.inc.php') : require_once('../../Users/VSpoe/mysqli_connect.inc.php');
-	mysqli_query($dbc, "UPDATE messages SET votes = votes + 1 WHERE id = {$_GET['id']}");
-	$result = mysqli_query($dbc, "SELECT votes FROM messages WHERE id = {$_GET['id']}");
+	mysqli_query($dbc, "UPDATE messages SET votes = votes + 1 WHERE id = {$_REQUEST['id']}");
+	$result = mysqli_query($dbc, "SELECT votes FROM messages WHERE id = {$_REQUEST['id']}");
 	$rows = mysqli_fetch_array($result, MYSQLI_NUM);
 	echo $rows[0];
 ?>
