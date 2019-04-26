85b1e1190859ce10a27a9b69cb09dbae4b593ad4
KnowledgeableKangaroo <31084854+KnowledgeableKangaroo@users.noreply.github.com>
2019-04-25 21:09:29 -0400

    Started adding topics

--- pages/topics.php
+++ pages/topics.php
@@ -3,5 +3,21 @@ $page_title = "Borum - Topics";
 include('includes/header.html');
 
 file_exists('../../mysqli_connect.inc.php') ? require_once('../../mysqli_connect.inc.php') : require_once('../../Users/VSpoe/mysqli_connect.inc.php');
+?>
 
+<?php 
+/* Here is where the fun begins */
+echo "<ul>";
+$query = "SELECT id, name FROM topics";
+$result = mysqli_query($dbc, $query);
+while ($row = mysqli_fetch_array($result, MYSQLI_NUM)) {
+	echo "<li><a href = '../Topics/$row[1]'>{$row[1]}</a></li>";
+}
+echo "</ul>";
+
+?>
+
+<?php
 include('includes/footer.html');
+?>
+
