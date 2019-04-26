0f7091d02656a5e3de918126ef1e7cfb2158d1eb
KnowledgeableKangaroo <31084854+KnowledgeableKangaroo@users.noreply.github.com>
2019-04-26 16:52:37 -0400

    Latest questions for that topic

--- pages/topic.php
+++ pages/topic.php
@@ -3,7 +3,7 @@
 	include('includes/login_functions.inc.php');
 
 	// Generate query for question's information
-	$query = 'SELECT name FROM topics WHERE name = "' . $_GET['topic'] . '"';
+	$query = 'SELECT id, name FROM topics WHERE name = "' . $_GET['topic'] . '"';
 	$result = mysqli_query($dbc, $query);
 	$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
 
@@ -11,8 +11,31 @@
 	include('includes/header.html');
 ?>
 	<h1><?php echo $row['name']; ?></h1>
-</div>
 	<?php
+		require('includes/pagination_functions.inc.php');
+
+		define('DISPLAY', 10); // Number of records to show per page
+
+		$pages = getPagesValue('id', 'messages', 'WHERE forum_id = ' . $row['id']);
+		$start = getStartValue();
+
+		$q = 'SELECT id, subject FROM messages WHERE forum_id = ' . $row['id'];
+		$result = performPaginationQuery($q, 'date_entered DESC', $start, $dbc);
+
+		echo "<table id = 'latest-questions'>";
+		while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) { // Loop through the records in an associative array
+
+			echo "
+			<tr><td align = \"left\"><a href = \"../Questions/{$row['id']}\">{$row['subject']}</a></td></tr>
+			";
+
+		}
+		echo "</table>";
+
+		mysqli_free_result($result);
+
+		setPreviousAndNextLinks('../Topics/' . $_GET['topic']);
+
 
 		mysqli_close($dbc);	
 		include('includes/footer.html');
