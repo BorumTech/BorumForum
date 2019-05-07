e662d1195d0b150f61db4096588639af078591da
KnowledgeableKangaroo <31084854+KnowledgeableKangaroo@users.noreply.github.com>
2019-05-07 19:57:17 -0400

    Moved forEach.call outside loadXMLDoc()

--- pages/question.php
+++ pages/question.php
@@ -1,10 +1,10 @@
 <?php
 	file_exists('../../mysqli_connect.inc.php') ? require_once('../../mysqli_connect.inc.php') : require_once('../../Users/VSpoe/mysqli_connect.inc.php');
 	// Generate query for question's information
-	$query = 'SELECT messages.id AS msg_id, messages.votes AS votes, messages.subject AS subject, messages.body AS ques_body, users.profile_picture AS ques_profile_pic, users.first_name AS ques_asker FROM messages JOIN users ON messages.user_id = users.id WHERE messages.id = ' . $_GET['id'];
+	$query = 'SELECT users.id AS usr_id, messages.id AS msg_id, messages.votes AS votes, messages.subject AS subject, messages.body AS ques_body, users.profile_picture AS ques_profile_pic, users.first_name AS ques_asker FROM messages JOIN users ON messages.user_id = users.id WHERE messages.id = ' . $_GET['id'];
 	$result = mysqli_query($dbc, $query);
 	$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-	$query2 = 'SELECT messages.id AS msg_id, messages.votes AS votes, messages.body AS msg_body, users.profile_picture AS profile, users.first_name AS fn FROM messages JOIN users ON users.id = messages.user_id WHERE parent_id = ' . $_GET['id'];
+	$query2 = 'SELECT messages.id AS msg_id, users.id AS usr_id, messages.votes AS votes, messages.body AS msg_body, users.profile_picture AS profile, users.first_name AS fn FROM messages JOIN users ON users.id = messages.user_id WHERE parent_id = ' . $_GET['id'];
 	$result2 = mysqli_query($dbc, $query2);
 
 	$page_title = $row['subject'];
@@ -21,13 +21,23 @@
 				<tr>
 					<td>
 					<?php 
-						
-						$ques_id = $row['msg_id'];	
-						$uparrow = '<svg aria-hidden="true" class="svg-icon m0 iconArrowUpLg" width="36" height="36" viewBox="0 0 36 36"><path d="M2 26h32L18 10z"></path></svg>';
-						$downarrow = '<svg aria-hidden="true" class="svg-icon m0 iconArrowDownLg" width="36" height="36" viewBox="0 0 36 36"><path d="M2 10h32L18 26z"></path></svg>';
+						function votedOnQuestion($msg_id, $vote) {
+							global $dbc;
+							if (isset($_COOKIE['id'])) {
+								$query = "SELECT id FROM `user-message-votes` WHERE user_id = {$_COOKIE['id']} AND message_id = $msg_id AND vote = $vote";
+								$result = mysqli_query($dbc, $query);
+								return mysqli_num_rows($result) >= 1;								
+							}
+
+						}
+						$ques_id = $row['msg_id'];							
+						$fillColor = votedOnQuestion($row['msg_id'], 1) ? 'lightgreen' : 'rgb(221, 221, 221)';
+						$uparrow = '<svg aria-hidden="true" class="svg-icon m0 iconArrowUpLg" width="36" height="36" viewBox="0 0 36 36"><path style = "fill:' . $fillColor . '" d="M2 26h32L18 10z"></path></svg>';
+						$fillColor = votedOnQuestion($row['msg_id'], -1);
+						$downarrow = '<svg aria-hidden="true" class="svg-icon m0 iconArrowDownLg" width="36" height="36" viewBox="0 0 36 36"><path style = "fill:' . $fillColor . '" d="M2 10h32L18 26z"></path></svg>';
 						$noAccountVoteUpBtn = "\t<button type = 'button' onclick = \"window.location.href = '/Login'\">$uparrow</button>\n";
 						$noAccountVoteDownBtn = "\t<button type = 'button' onclick = \"window.location.href = '/Login'\">$downarrow</button>\n";
-						
+
 						$voteupbtn = isset($_COOKIE['id']) ? "<button type = 'button' id = 'ques-vote-up-btn' onclick = \"loadXMLDoc('up', {$_COOKIE['id']}, $ques_id, 'ques-vote-count')\">$uparrow</button>\n" : $noAccountVoteUpBtn;
 						$votedownbtn = isset($_COOKIE['id']) ? "<button type = 'button' id = 'ques-vote-down-btn' onclick = \"loadXMLDoc('down', {$_COOKIE['id']}, $ques_id, 'ques-vote-count')\">$downarrow</button>\n" : $noAccountVoteDownBtn;
 
@@ -49,6 +59,7 @@
 
 					$counter = 1;
 					while ($row2 = mysqli_fetch_array($result2, MYSQLI_ASSOC)) {
+						$fillColor = votedOnQuestion($row2['usr_id'], $row2['msg_id']) ? 'red' : 'rgb(221, 221, 221)';
 						$voteupbtn = isset($_COOKIE['id']) ? "\t<button type = 'button' onclick = \"loadXMLDoc('up', {$_COOKIE['id']}, {$row2['msg_id']}, 'ans-$counter-vote-count')\">$uparrow</button>\n" : $noAccountVoteUpBtn;
 						$votedownbtn = isset($_COOKIE['id']) ? "\t\t<button type = 'button' onclick = \"loadXMLDoc('down', {$_COOKIE['id']}, {$row2['msg_id']}, 'ans-$counter-vote-count')\">$downarrow</button>\n" : $noAccountVoteDownBtn;
 						echo "<tr>";
