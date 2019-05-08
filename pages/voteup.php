8bedba85bb34540c5cc51cc4728e97680d69d381
KnowledgeableKangaroo <31084854+KnowledgeableKangaroo@users.noreply.github.com>
2019-05-08 19:50:21 -0400

    Add "ORDER BY id DESC LIMIT 1"
    Ignores previous votes

--- pages/voteup.php
+++ pages/voteup.php
@@ -11,10 +11,10 @@
 	}
 
 	// Check whether the user already voted on this message
-	$firstq = "SELECT id FROM `user-message-votes` WHERE user_id = {$_GET['user_id']} AND message_id = {$_GET['message_id']} AND vote = 1";
+	$firstq = "SELECT id FROM `user-message-votes` WHERE user_id = {$_GET['user_id']} AND message_id = {$_GET['message_id']} AND vote = 1 ORDER BY id DESC LIMIT 1";
 	$firstr = mysqli_query($dbc, $firstq);
 	if (mysqli_num_rows($firstr) >= 1) {
-		// If they already voted this question up
+		// If they just voted this question up and want to undo their vote
 		echo getVotes();
 
 		// TODO: Implement undoing the vote up
@@ -27,10 +27,7 @@
 
 	 	$q2 = "INSERT INTO `user-message-votes` (user_id, message_id, vote) VALUES ({$_GET['user_id']}, {$_GET['message_id']}, 1)";
 	 	$r2 = mysqli_query($dbc, $q2);
-	 	$q3 = "UPDATE messages SET votes = votes + 1 WHERE id = {$_REQUEST['message_id']}";
-	 	$r3 = mysqli_query($dbc, $q3);
 	 	if($changedVote) {
-	 		mysqli_query($dbc, $q3);
 	 		mysqli_query($dbc, $q2);
 	 	}
 	 	$result = getVotes();
