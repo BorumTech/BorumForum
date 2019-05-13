ff7b016659367a5458f8f2e1cbfab915f16e910b
KnowledgeableKangaroo <31084854+KnowledgeableKangaroo@users.noreply.github.com>
2019-05-13 16:23:15 -0400

    Use flexbox for topics
    Create new style sheet called flex.css
    Apply flex properties to the div (originally ul) and its child elements (<li>s) that cause four items to appear per row, at most

--- pages/topics.php
+++ pages/topics.php
@@ -5,15 +5,29 @@ include('includes/header.html');
 file_exists('../../mysqli_connect.inc.php') ? require_once('../../mysqli_connect.inc.php') : require_once('../../Users/VSpoe/mysqli_connect.inc.php');
 ?>
 
+<h1>Topics</h1>
+
+<script>
+	let flex = document.createElement("link");
+	flex.type = "text/css";
+	flex.rel = "stylesheet";
+	flex.href = "../css/flex.css";
+	document.head.appendChild(flex);
+</script>
+
 <?php 
 /* Here is where the fun begins */
-echo "<ul id = 'topic-list'>";
+echo "<div class = 'flex-box' id = 'topic-list'>";
 $query = "SELECT id, name FROM topics";
 $result = mysqli_query($dbc, $query);
 while ($row = mysqli_fetch_array($result, MYSQLI_NUM)) {
-	echo "<li><a href = '../Topics/$row[1]'>{$row[1]}</a></li>";
+	echo "<li class = 'flex-item'><a href = '../Topics/$row[1]'>{$row[1]}</a></li>";
 }
-echo "</ul>";
+echo "</div>";
+
+if (ISADMIN) {
+	echo "<br><a href = 'pages/create_topic.php'>Add New Topic</a>";
+}
 
 ?>
 
