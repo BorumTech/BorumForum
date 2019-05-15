34f18f95db81a71cd0b77d733b0b177c0362b117
KnowledgeableKangaroo <31084854+KnowledgeableKangaroo@users.noreply.github.com>
2019-05-14 20:42:23 -0400

    Show days ago on questions page

--- pages/questions.php
+++ pages/questions.php
@@ -14,14 +14,22 @@ define('DISPLAY', 10); // Number of records to show per page
 $pages = getPagesValue('id', 'messages', 'WHERE parent_id = 0');
 $start = getStartValue();
 
-$q = 'SELECT id, subject FROM messages WHERE parent_id = 0';
+$q = '
+SELECT messages.id, messages.subject, DATEDIFF(NOW(), messages.date_entered) AS date_posted, topics.name 
+FROM messages 
+JOIN topics
+ON messages.forum_id = topics.id
+WHERE parent_id = 0';
 $result = performPaginationQuery($q, 'date_entered DESC', $start, $dbc);
 
 echo "<table id = 'latest-questions'>";
 while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) { // Loop through the records in an associative array
 
 	echo "
-	<tr><td align = \"left\"><a href = \"Questions/{$row['id']}\">{$row['subject']}</a></td></tr>
+	<tr>
+	<td align = \"left\"><a href = \"Questions/{$row['id']}\">{$row['subject']}</a></td>
+	<td align = \"right\" class = 'date-diff' style = 'font-style: italic'>{$row['date_posted']} days ago</td>
+	</tr>
 	";
 
 }
