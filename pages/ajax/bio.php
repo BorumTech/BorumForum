e113b5e1ad17e3ca779515160489f890bfdf9d0d
KnowledgeableKangaroo <31084854+KnowledgeableKangaroo@users.noreply.github.com>
2019-06-02 16:16:01 -0400

    Toggle between "Edit" and "Save"

--- pages/ajax/bio.php
+++ pages/ajax/bio.php
@@ -11,6 +11,10 @@ switch ($_GET['func']) {
 		break;
 	case "update":
 		$query = "UPDATE bio FROM users SET bio = WHERE id = {$_GET['id']} LIMIT 1";
+		$query = "SELECT bio FROM users WHERE id = {$_GET['id']} LIMIT 1";
+		$result = mysqli_query($dbc, $query);
+		$row = mysqli_fetch_array($result, MYSQLI_ASSOC); 
+		echo $row['bio'];	
 		break;
 	default:
 		break;
--- scripts/script.js
+++ scripts/script.js
@@ -2,18 +2,23 @@ function deleteSubmission() {
 	
 }
 
-function editBio(id, func) {
+function editBio(id) {
 	/*
-	This function accomplishes three tasks
+	This function accomplishes two tasks
 		* Outputs the current bio in a textarea through asynchronous programming
-		* Updates the new bio to the database
-		* Outputs the new bio in an output element through asynchronous programming
-	It uses conditionals to determine which of the three tasks it must accomplish
-	This function takes two parameters
+		* Updates the new bio to the database and outputs the new bio in an output element through asynchronous programming
+	It uses conditionals to determine which task it must accomplish
+	This function takes one
 		* id (integer), the id of the user who is changing his or her bio
-		* func (string), takes either 'select' or 'update'
 	*/
-	// Output the current bio in a textarea
+
+	let func = "select";
+
+	const bioEl = document.getElementById('bio');
+	const isOutput = bioEl.tagName == 'OUTPUT';
+	const isTextarea = bioEl.tagName == 'TEXTAREA';
+	func = isTextarea ? "update" : "select";
+
 	let xhr;
 	if (window.XMLHttpRequest){ // code for IE7+, Firefox, Chrome, Opera, Safari
 		xhr = new XMLHttpRequest();
@@ -22,8 +27,13 @@ function editBio(id, func) {
 	}
 
 	xhr.onreadystatechange = function() {
-		if (xhr.readyState == 4 && xhr.status == 200) {
-			document.getElementById('bio').outerHTML = `<textarea id = 'bio'>${xhr.responseText}</textarea>`;	
+		if (xhr.readyState == 4 && xhr.status == 200) {	
+			const outputN = `<output id="bio">${xhr.responseText}</output>`;
+			const textareaN = `<textarea id="bio">${xhr.responseText}</textarea>`;
+
+			bioEl.nextElementSibling.innerHTML = isOutput ? "Save" : "Edit Bio";	
+
+			bioEl.outerHTML = isTextarea ? outputN : textareaN;
 		}
 	}
 
