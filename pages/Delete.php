f6b34faf7cd978bbb8d5c62b40f43df015d02e9c
KnowledgeableKangaroo <31084854+KnowledgeableKangaroo@users.noreply.github.com>
2019-06-14 18:24:46 -0400

    Update Delete.php

--- pages/Delete.php
+++ pages/Delete.php
@@ -33,7 +33,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 		<label>Are you sure you want to delete this post?</label>
 		<input name = "confirm" type = "radio" id = "yes" value = "Yes">
 		<label style = "font-weight: 400" for = "yes">Yes</label>
-		<input name = "confirm" type = "radio" value = "No" id = 'no'>
+		<input name = "confirm" type = "radio" value = "no" id = 'no'>
 		<label for = "no" style = "font-weight: 400">No</label>
 	</p>
 	<input type = "submit" value = "Confirm Deletion">
