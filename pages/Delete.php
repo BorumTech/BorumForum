8e7b3cbe73103771daf7fd4552b0abd68ca863a3
KnowledgeableKangaroo <31084854+KnowledgeableKangaroo@users.noreply.github.com>
2019-06-14 18:23:50 -0400

    Update Delete.php

--- pages/Delete.php
+++ pages/Delete.php
@@ -8,7 +8,7 @@ $query = "SELECT subject, body, user_id FROM messages WHERE id = {$_GET['id']}";
 $result = mysqli_query($dbc, $query);
 $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
 
-if (!LOGGEDIN && $row['user_id'] !== $_GET['id']) { // Make sure user is author of the question by redirecting everyone else
+if (!LOGGEDIN || $row['user_id'] !== $_GET['id']) { // Make sure user is author of the question by redirecting everyone else
 	redirect_user();
 }
 
