4d25cc30929e3ac3e5e07805219cbfd19c4dea78
KnowledgeableKangaroo <31084854+KnowledgeableKangaroo@users.noreply.github.com>
2019-06-15 22:11:19 -0400

    Delete followtopic.php

--- pages/ajax/followtopic.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$q = "INSERT INTO `followed-topics` (user_id, topic_id) VALUES ({$_POST['user_id']}, {$_POST['topic_id']})";
-$r = mysqli_query($dbc, $q);
-if (mysqli_affected_rows($r) == 1) {
-
-}
-
-?>
