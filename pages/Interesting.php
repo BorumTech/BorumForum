0b13a1d4fb98c323c0028f17fc6c89d696a24fe5
KnowledgeableKangaroo <31084854+KnowledgeableKangaroo@users.noreply.github.com>
2019-07-15 22:09:04 -0400

    Update Interesting.php

--- pages/Interesting.php
+++ pages/Interesting.php
@@ -3,7 +3,17 @@
 $page_title = "Interesting Questions";
 @require_once('includes/header.html');
 
+?>
 
+<div class = "col-sm-10">
+<h1>This page has not been fully designed. </h1>
+<script>
+	setTimeout(function() {
+		window.location.href = "/";
+	}, 1000);
+</script>
+
+<?php
 
 @include_once('includes/footer.html');
 ?>
