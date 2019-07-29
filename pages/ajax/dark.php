c36b1576b040c9375693a7d887dbfd4c40557f66
KnowledgeableKangaroo <31084854+KnowledgeableKangaroo@users.noreply.github.com>
2019-07-28 21:00:40 -0400

    Set dark mode as default
    Use ajax to set 0 or 1 in the dark column of the users table in the firstborumdatabase database on the MySQL server.
    The ajax function here is called changeTheme() and is located in scripts/settings.js

---
+++ pages/ajax/dark.php
@@ -0,0 +1,13 @@
+<?php 
+file_exists('../../../mysqli_connect.inc.php') ? require_once('../../../mysqli_connect.inc.php') : require_once('../../../Users/VSpoe/mysqli_connect.inc.php');
+
+$q = "UPDATE users SET dark = {$_COOKIE['dark']} WHERE id = {$_GET['id']}";
+mysqli_query($dbc, $q);
+
+$q = "SELECT dark FROM users WHERE id = {$_GET['id']}";
+$r = mysqli_query($dbc, $q);
+$row = mysqli_fetch_array($r, MYSQLI_ASSOC);
+echo "Dark mode set to {$row['dark']}.";
+
+?>
+
--- pages/login.php
+++ pages/login.php
@@ -20,6 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 		setcookie('id', $data['id'], time()+3600, '/', '', 0, 0);
 		setcookie('first_name', $data['first_name'], time()+3600, '/', '', 0, 0);
 		setcookie('last_name', $data['last_name'], time()+3600, '/', '', 0, 0);
+		setcookie('dark', $data['dark'], time() + 3600, '/', '', 0, 0);
 
 		// Redirect:
 		redirect_user('../Welcome');
