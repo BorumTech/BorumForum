bd176e85d3fa9c7893e9558767cca31e4647d309
KnowledgeableKangaroo <31084854+KnowledgeableKangaroo@users.noreply.github.com>
2019-08-01 22:48:02 -0400

    Copy and paste all answer code into answer.php

--- pages/ajax/answer.php
+++ pages/ajax/answer.php
@@ -17,7 +17,74 @@ if ($num == 0) { // No answers that match this one (no duplicates) on the curren
 }
 
 // Return the answer to the question that has just been posted
-echo "<tr><td>$ans</td></tr>";
+function getUpArrow() {
+	global $fillColor;
+	return '<svg aria-hidden="true" class="svg-icon m0 iconArrowUpLg" width="36" height="36" viewBox="0 0 36 36"><path style = "fill:' . $fillColor . '" d="M2 26h32L18 10z"></path></svg>';
+}
 
+function getDownArrow() {
+	global $fillColor;
+	return '<svg aria-hidden="true" class="svg-icon m0 iconArrowDownLg" width="36" height="36" viewBox="0 0 36 36"><path style = "fill:' . $fillColor . '" d="M2 10h32L18 26z"></path></svg>';
+}
+
+$ansquery = '
+SELECT messages.id AS msg_id, users.id AS usr_id, messages.body AS msg_body, users.profile_picture AS profile, users.first_name AS fn
+FROM messages 
+JOIN users 
+ON users.id = messages.user_id';
+$ansresult = mysqli_query($dbc, $ansquery);
+$ansrow = mysqli_fetch_array($ansresult, MYSQLI_ASSOC);
+$ans_id = $ansrow['msg_id'];
+$fillColor = votedOnQuestion($ansrow['msg_id'], 1) ? 'lightgreen' : 'rgb(221, 221, 221)';
+$uparrow = getUpArrow();
+$noAccountVoteUpBtn = getNoAccountButton($uparrow);
+
+$fillColor = votedOnQuestion($ansrow['msg_id'], -1) ? 'lightgreen' : 'rgb(221, 221, 221)';
+$downarrow = getDownArrow();
+$noAccountVoteDownBtn = getNoAccountButton($downarrow);
+
+$voteupbtn = isset($_COOKIE['id']) ? "\t<button type = 'button' onclick = \"loadXMLDoc('up', {$_COOKIE['id']}, {$ansrow['msg_id']}, 'ans-$counter-vote-count')\">$uparrow</button>\n" : $noAccountVoteUpBtn;
+$votedownbtn = isset($_COOKIE['id']) ? "\t\t<button type = 'button' onclick = \"loadXMLDoc('down', {$_COOKIE['id']}, {$ansrow['msg_id']}, 'ans-$counter-vote-count')\">$downarrow</button>\n" : $noAccountVoteDownBtn; 
+
+echo "<tr>";
+echo "<td>";
+echo $voteupbtn;
+
+	$voteCount = $ansrow['votes'] == null ? 0 : $ansrow['votes'];
+
+echo "\t\t<br><div class = 'vote-counter' id = 'ans-$counter-vote-count'>$voteCount</div>";
+echo $votedownbtn;
+echo "</td>";
+// Generate query for answers' information
+echo "<td>";
+echo "\t\t<p id = \"{$ansrow['msg_id']}\" class = 'ans-body'>{$ansrow['msg_body']}</p>\n";
+echo "</td>";
+echo "</tr>\n";
+echo "<tr class = 'user-profile-container'>";
+	if (LOGGEDIN && $_COOKIE['id'] === $ansrow['usr_id']) {
+		$what_to_echo = $ansrow['msg_id'] . '/Edit';
+
+		echo '<td class = "modify-links">';
+		echo "<a href = '$what_to_echo'>Edit</a> ";
+
+		$what_to_echo = $ansrow['msg_id'] . '/Delete';
+
+		echo "<a href = '$what_to_echo'>Delete</a>";
+		echo "</td>";
+	}
+	echo "<td colspan = \"2\" class = \"question-poster\">
+			<div>
+				<a href = '/Users/{$ansrow['usr_id']}'>
+					<span>{$ansrow['fn']}</span>
+				</a>
+				<a href = '/Users/{$ansrow['usr_id']}'>
+					<img height = '30' src = \"../pages/show_image.php?image={row2['profile']}\">
+				</a>
+			</div>	
+		</td>
+	</tr>";
+/*
+
+*/
 ?>
 
