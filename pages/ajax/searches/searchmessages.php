7af2e32d0d8b40ee5020d565f25edeab289aa122
KnowledgeableKangaroo <31084854+KnowledgeableKangaroo@users.noreply.github.com>
2019-10-09 17:10:24 -0400

    Add more styles + Add Help page

--- pages/ajax/searches/searchmessages.php
+++ pages/ajax/searches/searchmessages.php
@@ -1,7 +1,7 @@
 <?php 
 
 // Connect to the db
-file_exists('../../../../mysqli_connect.inc.php') ? require_once('../../../../mysqli_connect.inc.php') : require_once('../../../../Users/VSpoe/mysqli_connect.inc.php');
+file_exists('../../../../mysqli_connect.inc.php') ? require_once('../../../../mysqli_connect.inc.php') : require_once('../../../../../mysqli_connect.inc.php');
 
 $searchq = "SELECT * FROM messages WHERE MATCH (subject, body) AGAINST (\"{$_POST['q']}\")";
 
@@ -10,7 +10,7 @@ $searchr = mysqli_query($dbc, $searchq);
 $searchresult = "";
 
 while ($searchrow = mysqli_fetch_array($searchr, MYSQLI_ASSOC)) {
-   	$searchresult .= "<h3>{$searchrow['subject']}</h3><p>{$searchrow['body']}</p>";
+   	$searchresult .= "<a href = \"/Questions/{$searchrow['id']}\"><h3>{$searchrow['subject']}</h3><p>{$searchrow['body']}</p></a>";
 }
 
 echo $searchresult;
