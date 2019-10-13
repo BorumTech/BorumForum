264e68bef88231f5ac7027800af404677c4172e5
KnowledgeableKangaroo <31084854+KnowledgeableKangaroo@users.noreply.github.com>
2019-10-12 22:37:26 -0400

    Update edit_user.php

--- pages/edit_user.php
+++ pages/edit_user.php
@@ -75,7 +75,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 			if (mysqli_affected_rows($dbc) == 1) { // If it ran OK
 				echo '<p>The user has been edited.</p>';
-				redirect_js('/');
+				redirect_js('/', 500);
 			} else { // If it did not run OK
 				// Public message
 				echo '<p class = "error">The user could not be edited due to a system error. We apologize for any inconvenience.</p>'; 
