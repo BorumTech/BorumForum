93f4d3b8c558b59132279456914f25d16cba6684
KnowledgeableKangaroo <31084854+KnowledgeableKangaroo@users.noreply.github.com>
2019-12-02 20:58:05 -0500

    Write My Cousin Vinny Movie Review
    Also add styles for all articles in articles.css (new file)

--- thesiliconvalleyjournal/pages/columns/Cinema/2019/11/24.php
+++ thesiliconvalleyjournal/pages/columns/Cinema/2019/11/24.php
@@ -1,6 +1,17 @@
 <?php 
 
+$page_title = "My Cousin Vinny Review: Lynn makes you Laugh";
 require('../../../../includes/header.html');
+?>
+
+<div class = "article">
+	<h1 class = "article-title"><span class = "movie-name">My Cousin Vinny</span> Review: Lynn makes you Laugh</h1>
+	<p>The 1992 film My Cousin Vinny is a comedy worth watching. The slow-paced 2.5 hour movie takes place in Alabama where two New Yorkers going back to college are accused of murder. Luckily, Bill Gambini has a loudmouth cousin who agrees to plead their case. Humorously, Vincent Gambini is an inexperienced lawyer who needs time to adapt to the Southern rules, manners, culture, and procedures. His actions during the court case are absolutely hysterical, but Gambini learns and does a great job. I find it absolutely necessary that every man, woman, and child above the age of twelve with a sense of humor watch this movie. </p>
+	<p>Director Jonathan Lynn does a great job developing the characters and plot while inserting hilarious moments into the film. The best actor was Fred Gwynne, who played the uncompromising Judge. The Judge’s nature causes Vinny to be held in contempt of court multiple times. </p>
+	<p>Joe Pesci plays the second lawyer of the two defendants. Pesci stutters convincingly to demonstrate his nervousness. Though he has a small role, Pesci’s role is an important one. </p>
+	<p>Overall, my advice to movie lovers is to see the movie. The movie is rated R due to profanity, but anyone aged 12 and up may watch My Cousin Vinny. The movie demonstrates the huge contrast between Northerners and Southerners in the United States, or at least Italian-American New Yorkers and Alabamans. For example, Mr. Gambini has never heard of grits, but the breakfast seems very popular in the region. My personal rating of the film is 96% or 4.5 stars. IMdB’s is 7.6/10. Regardless, see My Cousin Vinny. </p>
+</div>
+<?php
 require('../../../../includes/footer.html');
 
 ?>
