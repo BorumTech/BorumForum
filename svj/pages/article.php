bab1a373d757349c449fbd64a36756fda2329f0c
KnowledgeableKangaroo <31084854+KnowledgeableKangaroo@users.noreply.github.com>
2019-12-22 21:39:58 -0500

    Made lots of progress on svj; fixed React issue

--- svj/pages/article.php
+++ svj/pages/article.php
@@ -1,10 +1,11 @@
 <?php 
 require('../../../svj_connect.inc.php');
-$result = mysqli_query($dbc, "SELECT * FROM articles");
+$result = mysqli_query($dbc, "SELECT * FROM articles WHERE title = \"{$_GET['name']}\"");
 $row = mysqli_fetch_array($result, MYSQLI_BOTH);
 
-$page_title = $row[1];
+$page_title = str_replace('-', " ", $row[1]);
 include('includes/header.html');
+echo '<div class = "article-body">' . $row[2] . '</div>';
 include('includes/footer.html');
 
 ?>
