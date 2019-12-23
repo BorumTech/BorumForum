5170595fe58e297dca85f47d4e78d3130f3556cc
KnowledgeableKangaroo <31084854+KnowledgeableKangaroo@users.noreply.github.com>
2019-12-23 17:35:02 -0500

    Create Borum Products page; solve #41

---
+++ products.bforborum.com/en-us/index.php
@@ -0,0 +1,17 @@
+<?php 
+require('header.html');
+?>
+<div class = "two product" id = "q-a">
+	<div class ="col-sm-6"><span>"There isn't another forum like it"</span></div>
+	<div class ="col-sm-6"><span>Borum Q & A</span></div>
+</div>
+<div class = "one product" id = "paint">
+	<div class ="col-sm-6"><span>"Better than any digital or physical paint pallete"</span></div>
+	<div class ="col-sm-6"><span>Borum Paint</span></div>
+</div>
+<div class = "two product" id = "weather">
+	<div class ="col-sm-6"><span>""</span></div>
+	<div class ="col-sm-6"><spam>Borum Weather</spam></div>
+</div>
+</body>
+</html>
---
+++ products.bforborum.com/index.php
@@ -0,0 +1,3 @@
+<?php 
+header("Location: http://products.bforborum.com/en-us");
+?>
