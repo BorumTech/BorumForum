90632bc640cae6158b26267bfe71f671fb6e1c7f
KnowledgeableKangaroo <31084854+KnowledgeableKangaroo@users.noreply.github.com>
2019-12-24 18:16:47 -0500

    Add fading effect

--- products.bforborum.com/en-us/index.php
+++ products.bforborum.com/en-us/index.php
@@ -5,13 +5,17 @@ require('header.html');
 	<div class ="col-sm-6"><span>"There isn't another forum like it"</span></div>
 	<div class ="col-sm-6"><span>Borum Q & A</span></div>
 </div>
-<div class = "one product" id = "paint">
-	<div class ="col-sm-6"><span>"Better than any digital or physical paint pallete"</span></div>
-	<div class ="col-sm-6"><span>Borum Paint</span></div>
+<div class = "one product" id = "svjournal">
+	<div class ="col-sm-6"><span>""</span></div>
+	<div class ="col-sm-6"><span>The Silicon Valley Journal</span></div>
 </div>
-<div class = "two product" id = "weather">
+<div class = "fadein two product" id = "weather">
 	<div class ="col-sm-6"><span>""</span></div>
 	<div class ="col-sm-6"><spam>Borum Weather</spam></div>
 </div>
-</body>
-</html>
+<div class = "fadein one product" id = "paint">
+	<div class ="col-sm-6"><span>"Better than any digital or physical paint pallete"</span></div>
+	<div class ="col-sm-6"><span>Borum Paint</span></div>
+</div>
+
+<?php require('footer.html'); ?>
