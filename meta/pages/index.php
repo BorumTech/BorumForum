587f2c2f94de3cd8ed630ee993d42f3830093edc
Varun <31084854+Borumer@users.noreply.github.com>
2020-01-30 20:42:23 -0500

    Add css files to CDN

--- meta/pages/index.php
+++ meta/pages/index.php
@@ -1,6 +1,16 @@
-<?php 
+<?php
 
 $page_title = "Meta Borum";
 require('includes/header.html');
+?>
+<div class = "col-sm-9">
+	<div class = "home-page">
+		<div class = "page-header">
+			<h1>Meta Borum</h1>
+			<p>A Q&A community about the Q&A community</p>
+		</div>
+	</div>
+</div>
+<?php
 include('includes/footer.html');
 ?>
