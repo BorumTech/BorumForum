2875551258dc62bd66a367a47e0573f9142b7666
Varun <31084854+Borumer@users.noreply.github.com>
2020-08-27 15:16:53 -0400

    Delete all pages in api folder

--- api/pages/ajax/edit.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-file_exists('../../../mysqli_connect.inc.php') ? require_once('../../../mysqli_connect.inc.php') : require_once('../../../../mysqli_connect.inc.php');
-
-session_start();
-
-
-?>
-
